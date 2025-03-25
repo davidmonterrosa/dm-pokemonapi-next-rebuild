28f37d2569d07466e3f1365d49497226f25575c2
davidmonterrosa <dmonterrosa99@gmail.com>
2025-03-25 14:57:59 -0700

    Initial commit

---
+++ src/components/PokemonDisplayArea.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Input } from './ui/input'
+
+const PokemonDisplayArea = () => {
+  return (
+    <div>
+        <Input type='text' placeholder='Search by name or Pokedex Number'/>
+        
+
+    </div>
+  )
+}
+
+export default PokemonDisplayArea
